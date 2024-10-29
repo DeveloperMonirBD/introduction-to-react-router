3481732cb4615405d290323245f495df57f40095
Monir <mrmonir0558@gmail.com>
2024-10-29 16:36:30 +0600

    first commit

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,11 @@
+
+
+const Footer = () => {
+    return (
+        <div>
+            <h2>Than you For browsing my website</h2>
+        </div>
+    );
+};
+
+export default Footer;
---
+++ src/main.jsx
@@ -0,0 +1,49 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Home from './components/Home/Home.jsx';
+import About from './components/About/About.jsx';
+import Contact from './components/Contact/Contact.jsx';
+import Users from './components/Users/Users.jsx';
+import UserDetails from './components/UserDetails/UserDetails.jsx';
+
+
+
+
+const router = createBrowserRouter([
+    {
+        path: '/',
+    element: <Home />,
+    children: [
+      {
+        path: '/about',
+        element: <About />
+      },
+      {
+        path: '/contact',
+        element: <Contact />
+      },
+      {
+        path: '/users',
+        loader: () => fetch('https://jsonplaceholder.typicode.com/users'),
+        element: <Users />
+      },
+      {
+        path: '/user/:userId',
+        loader: ({params}) => fetch(`https://jsonplaceholder.typicode.com/users/${params.userId}`),
+        element: <UserDetails />
+      }
+      
+    
+        ]
+  },
+]);
+
+
+createRoot(document.getElementById('root')).render(
+    <StrictMode>
+    <RouterProvider router={router} />
+    </StrictMode>
+);
